76603e1080e7d4a586de93a2a8ac09d5d93d20ae
DzmitryBandarenka <stibaslo.d@gmail.com>
2020-10-12 19:07:57 +0300

    feat: add Identity Login Authentication

---
+++ src/Filmary.Web/ViewModels/LoginViewModel.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Filmary.Web.ViewModels
+{
+    public class LoginViewModel
+    {
+        [Required]
+        [Display(Name = "Логин")]
+        public string UserName { get; set; }
+
+        [Required]
+        [DataType(DataType.Password)]
+        [Display(Name = "Пароль")]
+        public string Password { get; set; }
+
+        [Display(Name = "Запомнить?")]
+        public bool RememberMe { get; set; }
+
+        public string ReturnUrl { get; set; }
+    }
+}
---
+++ src/Filmary.Web/ViewModels/RegisterViewModel.cs
@@ -0,0 +1,48 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace Filmary.Web.ViewModels
+{
+    /// <summary>
+    /// Register model
+    /// </summary>
+    public class RegisterViewModel
+    {
+        /// <summary>
+        /// Email
+        /// </summary>
+        [Required]
+        [Display(Name = "Email")]
+        public string Email { get; set; }
+
+        /// <summary>
+        /// Phone number
+        /// </summary>
+        [Required]
+        [Display(Name = "Номер телефона")]
+        public string PhoneNumber { get; set; }
+
+        /// <summary>
+        /// User name
+        /// </summary>
+        [Required]
+        [Display(Name = "Имя пользователя")]
+        public string UserName { get; set; }
+
+        /// <summary>
+        /// Password
+        /// </summary>
+        [Required]
+        [DataType(DataType.Password)]
+        [Display(Name = "Пароль")]
+        public string Password { get; set; }
+
+        /// <summary>
+        /// Confirm password
+        /// </summary>
+        [Required]
+        [Compare("Password", ErrorMessage = "Пароли не совпадают")]
+        [DataType(DataType.Password)]
+        [Display(Name = "Подтвердить пароль")]
+        public string PasswordConfirm { get; set; }
+    }
+}
