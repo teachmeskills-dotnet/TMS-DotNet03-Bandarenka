c476d202c8eca05de51eb8a9403c3acc902fcc88
DzmitryBandarenka <stibaslo.d@gmail.com>
2020-10-22 14:42:11 +0300

    fix: add migration

---
+++ src/Filmary.DAL/Migrations/20201022105926_fixDB.cs
@@ -0,0 +1,17 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace Filmary.DAL.Migrations
+{
+    public partial class fixDB : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+
+        }
+    }
+}
